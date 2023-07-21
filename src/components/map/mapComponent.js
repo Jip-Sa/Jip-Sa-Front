6b8784b7f43874374808452af4029b1a1f5c7a54
jihwan <dhwlghksy@gmail.com>
2023-07-22 02:29:22 +0900

    FEAT) Make marker with searching and click

--- src/components/map/mapComponent.js
+++ src/components/map/mapComponent.js
@@ -1,43 +1,31 @@
-import React, { useEffect } from "react";
-import MarkerInfoWindow from "./MarkerInfoWindow";
-import ReactDOMServer from "react-dom/server";
+import React, { useEffect, useState } from "react";
+import MarkerInfoWindow from "./markerInfoWindow";
 
-const MapComponent = () => {
+const MapComponent = ({ location }) => {
+  const [initialized, setInitialized] = useState(false);
+  const [mapObject, setMapObject] = useState(null);
+  const [infoWindow, setInfoWindowObject] = useState(null);
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
       "https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=n6l9jjqqf3&submodules=geocoder";
     script.async = true;
     document.body.appendChild(script);
-    const title = "Test";
     script.onload = () => {
       const mapOptions = {
         center: new window.naver.maps.LatLng(37.3595704, 127.105399),
         zoom: 10,
       };
-      const map = new window.naver.maps.Map("map", mapOptions);
+      setMapObject(new window.naver.maps.Map("map", mapOptions));
 
-      const markerOptions = {
-        position: new window.naver.maps.LatLng(37.3595704, 127.105399),
-        title: title,
-        map: map,
-      };
-      var marker = new window.naver.maps.Marker(markerOptions);
-      console.log(
-        `This is the clickable of markger : ${marker.getClickable()}`
+      setInfoWindowObject(
+        new window.naver.maps.InfoWindow({
+          anchorSkew: true,
+        })
       );
-
-      const componentAsString = ReactDOMServer.renderToString(
-        <MarkerInfoWindow title={title} />
-      );
-      var infoWindow = new window.naver.maps.InfoWindow({
-        content: componentAsString,
-      });
-      window.naver.maps.Event.addListener(
-        marker,
-        "click",
-        onMarkerClick(marker, infoWindow)
-      );
+      console.log(`load Finish`);
+      setInitialized(true);
     };
 
     return () => {
@@ -45,17 +33,188 @@ const MapComponent = () => {
     };
   }, []);
 
-  return <div id="map" style={{ width: "100%", height: "100vh" }} />;
+  useEffect(() => {
+    if (initialized && location !== "") {
+      console.log("Before Initialize");
+      window.naver.maps.Service.geocode(
+        {
+          query: location,
+        },
+        function (status, response) {
+          if (status === window.naver.maps.Service.Status.ERROR) {
+            return alert("Something Wrong!");
+          }
 
-  function onMarkerClick(marker, infoWindow) {
-    return function (e) {
-      console.log(marker.title);
-      if (infoWindow.getMap()) {
-        infoWindow.close();
-      } else {
-        infoWindow.open(marker.getMap(), marker);
+          if (response.v2.meta.totalCount === 0) {
+            return alert("totalCount" + response.v2.meta.totalCount);
+          }
+
+          var htmlAddresses = [],
+            item = response.v2.addresses[0],
+            point = new window.naver.maps.Point(item.x, item.y);
+
+          if (item.roadAddress) {
+            htmlAddresses.push("[도로명 주소] " + item.roadAddress);
+          }
+
+          if (item.jibunAddress) {
+            htmlAddresses.push("[지번 주소] " + item.jibunAddress);
+          }
+
+          if (item.englishAddress) {
+            htmlAddresses.push("[영문명 주소] " + item.englishAddress);
+          }
+
+          infoWindow.setContent(
+            [
+              '<div style="padding:10px;min-width:200px;line-height:150%;">',
+              '<h4 style="margin-top:5px;">검색 주소 : ' +
+                location +
+                "</h4><br />",
+              htmlAddresses.join("<br />"),
+              "</div>",
+            ].join("\n")
+          );
+
+          mapObject.setCenter(point);
+          infoWindow.open(mapObject, point);
+        }
+      );
+    }
+  }, [location, mapObject, infoWindow, initialized]);
+
+  const getMapClick = (e) => {
+    mapObject.addListener("click", function (e) {
+      searchCoordinateToAddress(mapObject, e.coord, infoWindow);
+    });
+  };
+
+  return (
+    <div
+      id="map"
+      onClick={getMapClick}
+      style={{ width: "100%", height: "100vh" }}
+    />
+  );
+
+  function searchCoordinateToAddress(map, latlng, infoWindow) {
+    infoWindow.close();
+
+    window.naver.maps.Service.reverseGeocode(
+      {
+        coords: latlng,
+        orders: [
+          window.naver.maps.Service.OrderType.ADDR,
+          window.naver.maps.Service.OrderType.ROAD_ADDR,
+        ].join(","),
+      },
+      function (status, response) {
+        if (status === window.naver.maps.Service.Status.ERROR) {
+          return alert("Something Wrong!");
+        }
+
+        var items = response.v2.results,
+          address = "",
+          htmlAddresses = [];
+
+        for (var i = 0, ii = items.length, item, addrType; i < ii; i++) {
+          item = items[i];
+          address = makeAddress(item) || "";
+          addrType = item.name === "roadaddr" ? "[도로명 주소]" : "[지번 주소]";
+
+          htmlAddresses.push(i + 1 + ". " + addrType + " " + address);
+        }
+
+        infoWindow.setContent(
+          [
+            '<div style="padding:10px;min-width:200px;line-height:150%;">',
+            '<h4 style="margin-top:5px;">검색 좌표</h4><br />',
+            htmlAddresses.join("<br />"),
+            "</div>",
+          ].join("\n")
+        );
+
+        infoWindow.open(map, latlng);
       }
-    };
+    );
+  }
+
+  function makeAddress(item) {
+    if (!item) {
+      return;
+    }
+
+    var name = item.name,
+      region = item.region,
+      land = item.land,
+      isRoadAddress = name === "roadaddr";
+
+    var sido = "",
+      sigugun = "",
+      dongmyun = "",
+      ri = "",
+      rest = "";
+
+    if (hasArea(region.area1)) {
+      sido = region.area1.name;
+    }
+
+    if (hasArea(region.area2)) {
+      sigugun = region.area2.name;
+    }
+
+    if (hasArea(region.area3)) {
+      dongmyun = region.area3.name;
+    }
+
+    if (hasArea(region.area4)) {
+      ri = region.area4.name;
+    }
+
+    if (land) {
+      if (hasData(land.number1)) {
+        if (hasData(land.type) && land.type === "2") {
+          rest += "산";
+        }
+
+        rest += land.number1;
+
+        if (hasData(land.number2)) {
+          rest += "-" + land.number2;
+        }
+      }
+
+      if (isRoadAddress === true) {
+        if (checkLastString(dongmyun, "면")) {
+          ri = land.name;
+        } else {
+          dongmyun = land.name;
+          ri = "";
+        }
+
+        if (hasAddition(land.addition0)) {
+          rest += " " + land.addition0.value;
+        }
+      }
+    }
+
+    return [sido, sigugun, dongmyun, ri, rest].join(" ");
+  }
+
+  function hasArea(area) {
+    return !!(area && area.name && area.name !== "");
+  }
+
+  function hasData(data) {
+    return !!(data && data !== "");
+  }
+
+  function checkLastString(word, lastString) {
+    return new RegExp(lastString + "$").test(word);
+  }
+
+  function hasAddition(addition) {
+    return !!(addition && addition.value);
   }
 };
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,22 @@
-import React from "react";
-import MapComponent from "../components/map/MapComponent";
+import React, { useState } from "react";
+import MapComponent from "../components/map/mapComponent";
+import SearchBar from "../components/map/searchBar";
 
 const MainPage = () => {
+  const [searchResult, setSearchResult] = useState(null);
+
+  const handleSearch = (query) => {
+    // 검색 요청 처리 로직
+    // 예를 들어, 네이버 지도 API를 사용하여 검색 결과를 가져올 수 있습니다.
+    // setSearchResult를 통해 검색 결과를 저장합니다.
+    console.log(`This is Query : ${query}`);
+    setSearchResult(query);
+  };
   return (
     <div>
-      <h1>Main Page</h1>
-      <p>This is the main content of the application.</p>
-      <MapComponent />
+      <h1>Map Page</h1>
+      <SearchBar onSearch={handleSearch} />
+      <MapComponent location={searchResult} />
     </div>
   );
 };
