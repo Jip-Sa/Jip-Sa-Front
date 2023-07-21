63a21467761ce506e881112681645113e11691b2
jihwan <dhwlghksy@gmail.com>
2023-07-21 18:41:08 +0900

    FEAT) Show info window when mark is clicked

--- src/components/map/mapComponent.js
+++ src/components/map/mapComponent.js
@@ -7,7 +7,7 @@ const MapComponent = () => {
       "https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=n6l9jjqqf3&submodules=geocoder";
     script.async = true;
     document.body.appendChild(script);
-
+    const title = "Test";
     script.onload = () => {
       const mapOptions = {
         center: new window.naver.maps.LatLng(37.3595704, 127.105399),
@@ -17,17 +17,24 @@ const MapComponent = () => {
 
       const markerOptions = {
         position: new window.naver.maps.LatLng(37.3595704, 127.105399),
-        title: "test Marker",
+        title: title,
         map: map,
       };
       var marker = new window.naver.maps.Marker(markerOptions);
       console.log(
         `This is the clickable of markger : ${marker.getClickable()}`
       );
+
+      var infoWindow = new window.naver.maps.InfoWindow({
+        content:
+          '<div style="width:150px;text-align:center;padding:10px;">The Letter is <b>"' +
+          title +
+          '"</b>.</div>',
+      });
       window.naver.maps.Event.addListener(
         marker,
         "click",
-        onMarkerClick(marker)
+        이게된다고(marker, infoWindow)
       );
     };
 
@@ -38,9 +45,14 @@ const MapComponent = () => {
 
   return <div id="map" style={{ width: "100%", height: "100vh" }} />;
 
-  function onMarkerClick(marker) {
+  function 이게된다고(marker, infoWindow) {
     return function (e) {
       console.log(marker.title);
+      if (infoWindow.getMap()) {
+        infoWindow.close();
+      } else {
+        infoWindow.open(marker.getMap(), marker);
+      }
     };
   }
 };
