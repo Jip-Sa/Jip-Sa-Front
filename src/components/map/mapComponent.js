edb0ffc4208982bc24efb3b55e4ddd93fd4066cc
jihwan <dhwlghksy@gmail.com>
2023-07-25 16:55:21 +0900

    FEAT) Show datas from tables on map

--- src/components/map/mapComponent.js
+++ src/components/map/mapComponent.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import BuildingInfoPage from "./buildingInfoPage";
 
 const MapComponent = ({ searchResults }) => {
   const [initialized, setInitialized] = useState(false);
   const [mapObject, setMapObject] = useState();
-  const [markers, setMakers] = useState([]);
+  const [geoObject, setGeoObject] = useState();
+  const [baseMarkers, setBaseMarkers] = useState();
+  const [searchedMarkers, setSearchedMarkers] = useState([]);
+  const [showBuildingInfo, setShowBuildingInfo] = useState(false);
+  const [mapDatas, setMapDatas] = useState([]);
 
   useEffect(() => {
     var container = document.getElementById("map");
@@ -23,9 +30,75 @@ const MapComponent = ({ searchResults }) => {
 
         const map = new window.kakao.maps.Map(container, options);
         setMapObject(map);
+        // 주소-좌표 변환 객체를 생성합니다
+        var geocoder = new window.kakao.maps.services.Geocoder();
+        setGeoObject(geocoder);
+
+        window.kakao.maps.event.addListener(
+          map,
+          "click",
+          function (mouseEvent) {
+            // 클릭한 위도, 경도 정보를 가져옵니다
+            var latlng = mouseEvent.latLng;
+
+            var message = "클릭한 위치의 위도는 " + latlng.getLat() + " 이고, ";
+            message += "경도는 " + latlng.getLng() + " 입니다";
+
+            console.log(message);
+            console.log(`Click Map`);
+            setShowBuildingInfo(false);
+          }
+        );
       }
     });
   }, []);
+
+  // "http://172.10.5.130:80/jipsa/api/v1/rentInfo-dong?gu=성동구&dong=용답동&jibun=229-1";
+  useEffect(() => {
+    const apiUrl =
+      "http://172.10.5.130:80/jipsa/api/v1/rentInfo-dong?gu=강남구&dong=대치동";
+
+    // Axios를 사용하여 GET 요청 보내기
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        console.log(response.data);
+
+        setMapDatas(response.data);
+      })
+      .catch((error) => {
+        console.error("데이터를 불러오는 데 실패했습니다:", error);
+      });
+  }, []);
+
+  // 거래 목록을 바탕으로 marker를 띄웁니다.
+  useEffect(() => {
+    // console.log(mapDatas.officetel);
+    if (
+      mapDatas !== null &&
+      mapDatas !== [] &&
+      mapDatas.officetel !== undefined
+    ) {
+      for (const item of Array.from(mapDatas.officetel)) {
+        // console.log(item);
+        const address = `서울시 ${item.gu} ${item.dong} ${item.jibun}`;
+        // 주소로 좌표를 검색합니다
+        console.log(address);
+        // geoObject.addressSearch(address, function (result, status) {
+        //   // 정상적으로 검색이 완료됐으면
+        //   if (status === window.kakao.maps.services.Status.OK) {
+        //     var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+
+        //     // 결과값으로 받은 위치를 마커로 표시합니다
+        //     var marker = new window.kakao.maps.Marker({
+        //       map: mapObject,
+        //       position: coords,
+        //     });
+        //   }
+        // });
+      }
+    }
+  }, [mapDatas]);
 
   useEffect(() => {
     if (initialized && searchResults !== []) {
@@ -41,6 +114,7 @@ const MapComponent = ({ searchResults }) => {
         var marker = new window.kakao.maps.Marker({
           map: mapObject,
           position: position,
+          clickable: true,
         });
 
         bounds.extend(position); // bound에 좌표를 추가합니다.
@@ -48,29 +122,42 @@ const MapComponent = ({ searchResults }) => {
         marker.setMap(mapObject); // 마커가 지도 위에 표시되도록 설정합니다
 
         newMarkers.push(marker); // 마커를 배열에 저장합니다.
+        window.kakao.maps.event.addListener(marker, "click", function () {
+          // 마커 위에 인포윈도우를 표시합니다
+          getMarkerClick();
+        });
       });
       mapObject.setBounds(bounds);
-      setMakers(newMarkers);
+      setSearchedMarkers(newMarkers);
     }
   }, [searchResults]);
 
   const removeMarkers = () => {
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
+    for (var i = 0; i < searchedMarkers.length; i++) {
+      searchedMarkers[i].setMap(null);
     }
-    setMakers([]);
+    setSearchedMarkers([]);
   };
 
-  const getMapClick = (e) => {
-    console.log(`click!`);
+  const getMarkerClick = (e) => {
+    console.log(`Click Marker`);
+    setShowBuildingInfo(true);
+  };
+
+  const onInfoClose = (e) => {
+    console.log(`set false`);
+    setShowBuildingInfo(false);
   };
 
   return (
-    <div
-      id="map"
-      onClick={getMapClick}
-      style={{ width: "100%", height: "100vh" }}
-    />
+    <div>
+      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
+      {showBuildingInfo &&
+        ReactDOM.createPortal(
+          <BuildingInfoPage />,
+          document.getElementById("building-info-root") // 새 창을 생성할 DOM 요소 지정
+        )}
+    </div>
   );
 };
 
---
+++ src/components/separator.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Separator = () => {
+  return <div style={{ borderBottom: "30px solid #ccc" }}></div>;
+};
+
+export default Separator;
